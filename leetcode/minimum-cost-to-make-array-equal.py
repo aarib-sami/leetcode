0d3f364f468569a21e3b8b7ac1b32c32e3c7b548
Aarib Sami <aaribsamii@gmail.com>
2025-07-02 20:37:31 -0400

    Update solution for minimum-cost-to-make-array-equal

--- leetcode/minimum-cost-to-make-array-equal.py
+++ leetcode/minimum-cost-to-make-array-equal.py
@@ -1,7 +1,7 @@
 class Solution:
     def minCost(self, nums: List[int], cost: List[int]) -> int:
         res = float('inf')
-        
+        l, r = 0, max(nums)-1
         def costToChange(price):
             costNeeded = 0
             for p in range(len(nums)):
@@ -9,7 +9,16 @@ class Solution:
                 costNeeded += difference * cost[p]
             return costNeeded
         
-        for i in range(0, max(nums)+1):
-            res = min(res, costToChange(i))
+        while l <= r:
+            m = (l + r) // 2
+            m1 = costToChange(m)
+            m2 = costToChange(m+1)
+
+            if m1 < m2:
+                r = m - 1
+                res = min(res, m1)
+            else:
+                res = min(res, m2)
+                l = m + 1
 
         return res
