4bed0e97c77caa13fb75f813d6268c8af2a883bf
Aarib Sami <aaribsamii@gmail.com>
2025-07-09 23:10:26 -0400

    Update solution for unique-paths-ii

--- leetcode/unique-paths-ii.py
+++ leetcode/unique-paths-ii.py
@@ -1,20 +1,20 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         ROWS, COLS = len(obstacleGrid), len(obstacleGrid[0])
-        res = 0
-        stack = []
-        stack.append([0, 0])
+        dp = {(ROWS-1, COLS-1) : 1}
 
-        while stack:
-            i, j = stack.pop()
-            if obstacleGrid[i][j] == 1:
-                continue
+        def dfs(i, j):
+            if (i not in range(ROWS) or 
+            j not in range(COLS) or
+            obstacleGrid[i][j] == 1):
+                return 0
             elif i == ROWS-1 and j == COLS-1:
-                res += 1
+                return 1
 
-            if i + 1 in range(ROWS):
-                stack.append([i+1, j])
-            if j + 1 in range(COLS):
-                stack.append([i, j+1])
+            if (i, j) in dp:
+                return dp[(i, j)]
 
-        return res
+            dp[(i, j)] = dfs(i + 1, j) + dfs(i, j + 1)
+            return dp[(i, j)]
+
+        return dfs(0, 0)
